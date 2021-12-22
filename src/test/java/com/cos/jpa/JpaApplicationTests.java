efe636754474faecc5a548da264815ab1dcfb2a6
Woonkil <woonkil70@gmail.com>
2021-12-22 15:20:56 +0900

    2021.12.22.15

--- src/test/java/com/cos/jpa/JpaApplicationTests.java
+++ src/test/java/com/cos/jpa/JpaApplicationTests.java
@@ -1,9 +1,10 @@
 package com.cos.jpa;
 
+import com.cos.photogram.PhotogramApplication;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes= PhotogramApplication.class)
 class JpaApplicationTests {
 
 	@Test
